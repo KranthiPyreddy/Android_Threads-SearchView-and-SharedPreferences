b44b74b865d91b5bcaa97a4562c5ec1b30c4c4c6
kranthi pyreddy <kra330@gmail.com>
2021-03-23 16:49:22 -0400

    Initial Commit

---
+++ app/src/main/java/com/codewithpk/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,14 @@
+package com.codewithpk.photogallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_gallery);
+    }
+}
